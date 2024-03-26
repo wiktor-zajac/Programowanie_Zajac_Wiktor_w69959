2017179c24f0a34a36e02d6f7762a8b7325073e6
Wiktor Zając <ogniko00@gmail.com>
2024-03-27 00:46:31 +0100

    Added: semicolons after arrow functions Added: implementation for additional tasks

--- Lab1/script.js
+++ Lab1/script.js
@@ -26,7 +26,7 @@ console.log(evenNumbersFrom0To100Arrray);
 const triangleArea = () => {
     const p = (a + b + c) / 2;
     return Math.sqrt(p * (p - a) * (p - b) * (p - c));
-}
+};
 console.log(triangleArea());
 document.querySelector('p#js-triangle-area').innerHTML = triangleArea();
 
@@ -64,12 +64,45 @@ document.querySelector('p#js-max-number').innerHTML = biggestOutOfThree;
 const gcd = (a, b) => {
     if (!b) return a;
     return gcd(b, a % b);
-}
+};
 document.querySelector('p#js-gcd').innerHTML = gcd(parseInt(x), parseInt(y));
 
 
 // Zadanie 10
+const timeAndDateStringBuilder = () => {
+    const date = new Date();
+    return {
+        Y: date.getFullYear(),
+        M: date.getMonth() + 1,
+        D: date.getDate(),
+        h: date.getHours(),
+        m: date.getMinutes(),
+        s: date.getSeconds(),
+    }
+};
 
+const timeAndDateHTMLRender = () => {
+    const dt = timeAndDateStringBuilder();
+    const out = `${dt.D}.${dt.M}.${dt.Y} ${dt.h}:${dt.m}:${dt.s}`;
+    document.querySelector('p#js-date-time').innerHTML = out;
+};
+timeAndDateHTMLRender();
+setInterval(timeAndDateHTMLRender, 1000);
 
 
 // Zadanie 11
+const gameHTMLNodeForm = document.querySelector('form#js-game');
+const gameHTMLNodeFormGuess = gameHTMLNodeForm.querySelector('input#js-game-guess');
+const randomNumber = Math.round(Math.random() * 100);
+let attemptsCounter = 0;
+gameHTMLNodeForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    attemptsCounter++;
+    const guess = gameHTMLNodeFormGuess.value;
+    if (guess == randomNumber) {
+        alert(`Odgatnięto poprawną liczbę! Wymagało to ${attemptsCounter} prób`);
+        window.location.reload();
+    }
+    else if (guess > randomNumber) alert('Mniejsze');
+    else alert('Większe');
+})
