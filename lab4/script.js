2b2bdf2e03ad0c99d25715981c313f8313096e2a
Wiktor Zając <wiktor.zajac85@gmail.com>
2024-03-28 20:45:51 +0100

    Added: description for colored buttons Added: missing CSS classes Added: cooldown to countdown Fixed: incorrect redirection in age check Fixed: list node append Fixed: incorrect selectors Fixed: class toggle Changed: Baguette is now hidden by default

--- lab4/script.js
+++ lab4/script.js
@@ -1,59 +1,68 @@
 const $ = (q) => document.querySelector(q);
 const $all = (q) => document.querySelectorAll(q);
+const sleep = async ms => new Promise(r => setTimeout(r, ms));
 
 // Zadanie 1
-// TODO: Dodać cooldown
-const countDownForNode = $('js-1-for');
-const countDownWhileNode = $('js-1-while');
+const countDownForNode = $('#js-1-for');
+const countDownWhileNode = $('#js-1-while');
+const zad1_for = async () => {
+    for (let i = 10; i >= 1; i--) {
+        await sleep(1000);
+        countDownForNode.innerHTML += `${i}<br>`;
+    }
+    countDownForNode.innerHTML += `Szczęśliwego nowego roku!`;
+};
 
-for (let i = 10; i <= 1; i--)
-    countDownForNode.innerHTML += `${i}<br>`;
-countDownForNode.innerHTML += `Szczęśliwego nowego roku!`;
-
-i = 10;
-while (i <= 1) {
-    countDownWhileNode.innerHTML += `${i}<br>`;
-    i--;
+const zad1_while = async () => {
+    i = 10;
+    while (i >= 1) {
+        await sleep(1000);
+        countDownWhileNode.innerHTML += `${i}<br>`;
+        i--;
+    }
+    countDownWhileNode.innerHTML += `Szczęśliwego nowego roku!`;
 }
-countDownWhileNode.innerHTML += `Szczęśliwego nowego roku!`;
 
+zad1_for();
+zad1_while();
 
 // Zadanie 2
 const silnia = (n) => !(n > 1) ? 1 : silnia(n - 1) * n;
 
 
 // Zadanie 3
-if (window.confirm('Czy jesteś pełnoletni?'))
+if (!window.confirm('Czy jesteś pełnoletni?'))
     window.open('strona2.html');
 
 
 // Zadanie 4
-// TODO: Dodać animacje dla klas w CSS
 const buttons = $all('.js-4-btn');
 buttons.forEach((v, k) => {
-    v.addEventListener('click', () => {v.toggleClass('js-4-anim-start')});
+    v.addEventListener('click', () => {v.classList.toggle('js-4-anim-start')});
 });
 
 
 // Zadanie 5
-// TODO: Dodać klasę hide w CSS
 const hideImgBtn = $('.js-5-btn');
 const hideImgImg = $('.js-5-img');
 
 hideImgBtn.addEventListener('click', () => {
-    hideImgImg.toggleClass('hide');
-    hideImgBtn.value == 'Pokaż!' ? 'Schowaj' : 'Pokaż!';
+    hideImgImg.classList.toggle('hide');
+    hideImgBtn.value = hideImgBtn.value == 'Schowaj' ? 'Pokaż!' : 'Schowaj';
 });
 
 
 // Zadanie 6
 const
-    addToListBtn = $('.js-6-btn'),
-    addToListTxt = $('.js-6-img'),
-    addToListList = $('.js-6-list');
+    addToListBtn = $('#js-6-btn'),
+    addToListTxt = $('#js-6-txt'),
+    addToListList = $('#js-6-list');
 
 addToListBtn.addEventListener('click', () => {
-    addToListList.appendChild(`<li>${addToListTxt.value}</li>`)
+    let node = document.createElement('li');
+    node.append(addToListTxt.value);
+    addToListList.appendChild(node);
+    addToListTxt.value = '';
 });
 
 
