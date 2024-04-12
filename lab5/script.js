19e55d2fa2844c63db5c9b58b78b8c8854ed1e56
Wiktor Zając <ogniko00@gmail.com>
2024-04-12 23:56:43 +0200

    I forgot to do this task but I think that it's complete

--- lab5/script.js
+++ lab5/script.js
@@ -1,116 +1,148 @@
-const $ = (q, s=document) => s.querySelector(q);
-// TODO: zamienić console.log na wyświetlanie w HTML i poformatować
+const $ = (q, s = document) => s.querySelector(q);
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
 
 // Zadanie 1
-// const tab1 = [];
-// for (i=0;i<=10;i++) {
-//     let x = Number(prompt('cyfra='));
-//     tab1.push(x);
-// }
-// let s = Number(prompt('szukana='));
+// TODO: uncomment before deploy
+const tab1 = [];
+for (let i = 0; i <= 10; i++) {
+    let x = Number(prompt(`cyfra[${i}]=`, i * 2));
+    tab1.push(x);
+}
+let s = Number(prompt('szukana=', 10));
 
-const tab1 = [3, 6, 9, 2, 7, 5, 8, 2, 1, 5, 8, 8, 5, 3];
-let s = 8;
-
-let found = tab1.filter((el) => el == s);
-console.log(`Cyfrę ${s} znaleziono ${found.length} razy`);
+let found = tab1.filter((el) => el == s).length;
+$('#js-1-answer').innerHTML = `Cyfrę ${s} znaleziono ${found} ` + (found == 1 ? 'raz' : 'razy');
+$('#js-1-nums').innerHTML = tab1;
 
 
 // Zadanie 2
-// TODO: walidacja
 const tab2 = [1, 2, 3, 4, 5, 6];
-let usersInput = Number(prompt('Podaj liczbę całkowitą'));
-tab2.splice(usersInput, 0, usersInput);
-console.log(tab2);
+let usersInput = 0;
+while (true) {
+    usersInput = Number(prompt('Podaj liczbę całkowitą (mniejszą od 7)', 5));
+    if (usersInput < 7) break;
+}
+tab2.splice(
+    usersInput - 1,
+    tab2.length - usersInput + 1,
+    usersInput, ...tab2.slice(usersInput - 1).map((v) => v + 1)
+);
+$('#js-2-user-input').innerHTML = usersInput;
+$('#js-2-state').innerHTML = tab2;
 
 
 // Zadanie 3
-const tab3 = tab1.reverse();
-$('#js-3').innerHTML = tab1 + ' | ' + tab3;
-
+$('#js-3-before').innerHTML = tab1;
+$('#js-3-after').innerHTML = tab1.reverse();
 
 // Zadanie 4
-const tab4 = Array.from({length: 50}, () => Math.floor(Math.random() * 100));
-$('#js-4').innerHTML = tab4;
+const arrayOfRandomNumbers = (length = 50, min = 0, max = 100) =>
+    Array.from({ length: length }, () => randomInt(min, max))
+const tab4 = arrayOfRandomNumbers();
+$('#js-4-answer').innerHTML = tab4;
 
 
 // Zadanie 5
 const tab5 = {
     sum: 0,
-    evenFoundIndexes: [],
-    mulBy3: 0,
-    addedIndex: 0,
-    avg: 0,
+    foundIndexesOfEvenNumbers: [],
+    multipliedByThree: 0,
+    indexOfAddedValue: 0,
+    average: 0,
     max: 0,
-    occurences: 0,
+    occurencesOfRandomValue: 0,
 }
 
-tab1.sum = tab1.reduce((a, b) => a+b, 0);
-tab1.filter((v, i) => {
-    if (v % 2 == 0)
-        tab5.evenFoundIndexes.push(i);
+
+tab5.sum = tab4.reduce((accumulator, value) => accumulator + value, 0);
+
+tab4.forEach((value, index) => {
+    if (value % 2 == 0) {
+        tab5.foundIndexesOfEvenNumbers.push(index);
+    }
 });
-tab5.mulBy3 = tab1.map((v) => v*3);
-tab1.push(69959);
-tab5.addedIndex = tab1.find(el => el == 69959);
-tab5.avg = (tab5.sum + 69959) / tab1.length;
-tab5.max = Math.max(tab1);
-tab5.occurences = tab1.filter((el) => el == 5);
+
+tab5.multipliedByThree = tab4.map((v) => v * 3);
+
+tab4.push(69959);
+tab5.indexOfAddedValue = tab4.findIndex((el) => el == 69959);
+
+tab5.average = (tab5.sum + 69959) / tab4.length;
+
+tab5.max = Math.max(...tab4);
+
+const seekedValue = randomInt(0, 100);
+tab5.occurencesOfRandomValue = tab4.filter((el) => el == seekedValue).length;
+
+
+$('#js-5-seeked-value').innerHTML = seekedValue;
+for (const property of Reflect.ownKeys(tab5)) {
+    $(`#js-5-${property}`).innerHTML = tab5[property];
+}
 
 
 // Zadanie 6
-let i = 0;
-const fib = [0, 1];
+const tab6 = [0, 1];
 
-for (i = 2; i <= 100; i++) 
-    fib.push(fib[i - 2] + fib[i - 1]);
+for (let i = 2; i <= 100; i++)
+    tab6.push(tab6[i - 2] + tab6[i - 1]);
+$('#js-6-answer').innerHTML = tab6;
 
 
 // Zadanie 7
-const nextBiggest = (arr) => {
-    let
-        max = -Infinity,
-        result = -Infinity;
-  
-    for (const value of arr) {
-        const nextResult = value;
-  
-        if (nextResult > max)
-            [result, max] = [max, nextResult];
-        else if (nextResult < max && nextResult > result)
-            result = nextResult;
+const sumOfTwoBiggestNumbers = (arr) => {
+    let max = 0, result = 0;
+
+    for (const el of arr) {
+        if (el > max)
+            [result, max] = [max, el];
+        else if (el < max && el > result)
+            result = el;
     }
-  
-    return [max, result];
+
+    return max + result;
 }
-const tab7 = nextBiggest(tab1);
-console.log(tab7);
+$('#js-7-answer').innerHTML = sumOfTwoBiggestNumbers(tab4);
 
 
 // Zadanie 8
-const tab8 = [...new Set(tab1)];
-console.log(tab8);
+$('#js-8-answer').innerHTML = [...new Set(tab4)];
 
 
 // Zadanie 9
+const primeCheckerInputNode = $('#js-9-num'),
+    primeCheckerOutput = $('#js-9-answer');
+primeCheckerInputNode.addEventListener('input', () => {
+    checkIfInputIsPrime();
+});
+
 const isPrime = (num) => {
-    for(let i = 2, s = Math.floor(Math.sqrt(num)); i <= s; i++) {
-        if(num % i === 0) return false;
+    sqrtOfNum = Math.floor(Math.sqrt(num));
+    for (let i = 2; i <= sqrtOfNum; i++) {
+        if (num % i === 0) return false;
     }
     return num > 1;
 };
-console.log(isPrime(69959));
+const checkIfInputIsPrime = () => {
+    primeCheckerOutput.innerHTML = isPrime(Number(primeCheckerInputNode.value)) ? 'TAK' : 'NIE';
+}
+checkIfInputIsPrime();
 
 
 // Zadanie 10
-let text10 = 'Ala ma kota a kot ma Alę';
-const tab10 = text10.split(' ');
-text10 = tab10.reverse().join(' ');
-console.log(text10);
+const reverseWordOrderInput = $('#js-10-input'),
+    reverseWordOrderOutput = $('#js-10-output');
+const reverseWordOrder = () => reverseWordOrderOutput.value = reverseWordOrderInput.value.split(' ').reverse().join(' ');
+reverseWordOrder();
+reverseWordOrderInput.addEventListener('input', () => {
+    reverseWordOrder();
+});
 
 
 // Zadanie 11
-const randomFromArray = (arr) => arr[Math.floor(Math.random()*arr.length)];
+const randomPasswordButton = $('#js-11-output');
+const randomFromArray = (arr) => arr[Math.floor(Math.random() * arr.length)];
 const randomPassword = (length = 16) => {
     const allChars = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890';
     let result = [];
@@ -118,34 +150,58 @@ const randomPassword = (length = 16) => {
         result.push(randomFromArray(allChars));
     return result.join('');
 }
-console.log(randomPassword());
+randomPasswordButton.value = randomPassword();
+$('#js-11-btn').addEventListener('click', () => randomPasswordButton.value = randomPassword());
 
 
 // Zadanie 12
-const tableNode = $('#js-12');
-for(let x = 1; x <= 10; x++) {
+const tableNode = $('#js-12-table');
+for (let x = 1; x <= 10; x++) {
     let row = tableNode.insertRow();
-    for(let y = 1; y <= 10; y++) {
-        row.insertCell().innerHTML = x*y;
+    for (let y = 1; y <= 10; y++) {
+        row.insertCell().innerHTML = x * y;
     }
 }
 
 
 // Zadanie 13
-const randomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
 const firstNames = ['Jan', 'Jakub', 'Sebastian', 'Wiktor', 'Piotr', 'Natalia', 'Anna', 'Ewa', 'Małgorzata'];
 const lastNames = ['Nowak', 'Kowalski', 'Zając', 'Królik', 'Dąb', 'Świerk', 'Brzoza', 'Bąk', 'Mucha'];
 const people = [];
+const AMOUNT_OF_PERSONAL_DETAILS_TO_GENERATE = 20;
 
-for (let i = 0; i <= 20; i++) {
+for (let i = 0; i <= AMOUNT_OF_PERSONAL_DETAILS_TO_GENERATE; i++) {
     const person = {
         firstName: randomFromArray(firstNames),
         lastName: randomFromArray(lastNames),
-        age: randomNumber(18, 80),
-        phoneNumber: randomNumber(5_000_000, 8_000_000),
+        age: randomInt(18, 80),
+        phoneNumber: randomInt(5_000_000, 8_000_000),
     }
+    people.push(person);
 }
+
+let amountOfRecordsAlreadyDisplayed = 0;
 
 const
     previewNum = $('#js-13-num'),
-    previewBtn = $('')
+    previewBtn = $('#js-13-btn'),
+    previewTBody = $('#js-13-tbody');
+
+previewBtn.addEventListener('click', () => {
+    const amountOfPeopleToDisplay = Number(previewNum.value);
+
+    for (let i = 0;
+        i < amountOfPeopleToDisplay && amountOfRecordsAlreadyDisplayed < AMOUNT_OF_PERSONAL_DETAILS_TO_GENERATE;
+        i++) {
+        const
+            row = previewTBody.insertRow(),
+            person = people[amountOfRecordsAlreadyDisplayed + i],
+            propertiesToDisplay = Reflect.ownKeys(person);
+
+        for (const property of propertiesToDisplay) {
+            const cell = row.insertCell();
+            cell.innerHTML = person[property];
+        }
+    }
+    amountOfRecordsAlreadyDisplayed += amountOfPeopleToDisplay;
+});
